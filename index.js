c04cf768cc9803b4db87a399515bb6ff61205442
y788zhan <yzhang@sugarcrm.com>
2016-12-14 17:17:52 -0800

    typo fix round 2

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function _insertCredentials(username, userhash) {
 }
 
 /**
- * Restores `cachedRecords` and `crendetials` to their initial states.
+ * Restores `cachedRecords` and `credentials` to their initial states.
  *
  * @private
  */
