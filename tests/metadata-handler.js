c994c606555b9bb1207614a3f3090429a930c5cf
Bob Wombat Hogg <rhogg@sugarcrm.com>
2017-05-15 10:13:39 -0700

    Fix test style to conform with eslint config 1.1.0
    Signed-off-by: Bob Wombat Hogg <rhogg@sugarcrm.com>

--- tests/metadata-handler.js
+++ tests/metadata-handler.js
@@ -37,7 +37,7 @@ describe('Metadata Handler', () => {
                 ];
                 _.each(types, (type) => {
                     let msg = `Fields of type ${type} are not supported. Please define them manually.`;
-                    expect(() => Meta.generateFieldValue({ type })).to.throw(msg);
+                    expect(() => Meta.generateFieldValue({type})).to.throw(msg);
                 });
             });
         });
@@ -45,7 +45,7 @@ describe('Metadata Handler', () => {
         describe('unrecognized types', () => {
             it('should throw an error', () => {
                 let msg = 'Field type i_am_not_a_real_type is not recognized.';
-                expect(() => Meta.generateFieldValue({ type: 'i_am_not_a_real_type' })).to.throw(msg);
+                expect(() => Meta.generateFieldValue({type: 'i_am_not_a_real_type'})).to.throw(msg);
             });
         });
 
@@ -61,7 +61,7 @@ describe('Metadata Handler', () => {
             });
 
             it('should have a default length of 30', () => {
-                let value = Meta.generateFieldValue({ type: 'varchar' });
+                let value = Meta.generateFieldValue({type: 'varchar'});
                 expect(value).to.have.lengthOf(30);
             });
 
@@ -77,7 +77,7 @@ describe('Metadata Handler', () => {
 
         describe('passwords', () => {
             it('should return a string', () => {
-                let value = Meta.generateFieldValue({ type: 'password' });
+                let value = Meta.generateFieldValue({type: 'password'});
                 expect(value).to.be.a.string;
             });
         });
@@ -94,26 +94,26 @@ describe('Metadata Handler', () => {
             });
 
             it('should have a default length of 30', () => {
-                let value = Meta.generateFieldValue({ type: 'char' });
+                let value = Meta.generateFieldValue({type: 'char'});
                 expect(value).to.have.lengthOf(30);
             });
         });
 
         describe('bools', () => {
             it('should return a boolean', () => {
-                expect(Meta.generateFieldValue({ type: 'bool' })).to.be.a.boolean;
+                expect(Meta.generateFieldValue({type: 'bool'})).to.be.a.boolean;
             });
         });
 
         describe('ints', () => {
             it('should return an integer with the proper number of digits', () => {
-                let value = Meta.generateFieldValue({ type: 'int', len: 4 });
+                let value = Meta.generateFieldValue({type: 'int', len: 4});
                 expect(Number.isInteger(value)).to.be.true;
                 expect(value).to.be.at.most(9999);
             });
 
             it('should only return a number with at most 5 digits', () => {
-                let value = Meta.generateFieldValue({ type: 'int', len: 6 });
+                let value = Meta.generateFieldValue({type: 'int', len: 6});
                 expect(Number.isInteger(value)).to.be.true;
                 expect(value).to.be.at.most(99999);
             });
@@ -121,7 +121,7 @@ describe('Metadata Handler', () => {
 
         describe('decimals', () => {
             it('should return a number with the proper number of digits', () => {
-                let value = Meta.generateFieldValue({ type: 'decimal', len: '5,2' });
+                let value = Meta.generateFieldValue({type: 'decimal', len: '5,2'});
                 expect(value).to.be.a.number;
                 // Number.toString() always uses a ".", even in European locales
                 let [intPart, decimalPart] = value.toString().split('.');
@@ -130,7 +130,7 @@ describe('Metadata Handler', () => {
             });
 
             it('should only return a number with at most 3 digits before and 2 after the decimal', () => {
-                let value = Meta.generateFieldValue({ type: 'decimal', len: '10,5' });
+                let value = Meta.generateFieldValue({type: 'decimal', len: '10,5'});
                 // Number.toString() always uses a ".", even in European locales
                 let [intPart, decimalPart] = value.toString().split('.');
                 expect(intPart.length).to.be.at.most(3);
@@ -140,7 +140,7 @@ describe('Metadata Handler', () => {
 
         describe('datetimes', () => {
             it('should return a Date', () => {
-                let value = Meta.generateFieldValue({ type: 'datetime' });
+                let value = Meta.generateFieldValue({type: 'datetime'});
                 expect(value instanceof Date).to.be.true;
             });
         });
@@ -148,7 +148,7 @@ describe('Metadata Handler', () => {
         describe('urls', () => {
             it('should return an HTTP(S) URL', () => {
                 let url = require('url');
-                let value = url.parse(Meta.generateFieldValue({ type: 'url' }));
+                let value = url.parse(Meta.generateFieldValue({type: 'url'}));
                 expect(value.protocol).to.have.string('http');
             });
 
@@ -174,14 +174,14 @@ describe('Metadata Handler', () => {
         describe('emails', () => {
             it('should return an email address', () => {
                 // validating emails is arduous. Just check it's word@domain.tld
-                let value = Meta.generateFieldValue({ type: 'email' });
+                let value = Meta.generateFieldValue({type: 'email'});
                 expect(value).to.match(/\w*@\w*\.\w*/);
             });
         });
 
         describe('phone numbers', () => {
             it('should return a string of digits', () => {
-                let value = Meta.generateFieldValue({ type: 'phone' });
+                let value = Meta.generateFieldValue({type: 'phone'});
                 expect(value).to.be.a.string;
                 expect(value).to.match(/\d*/);
             });
@@ -199,25 +199,25 @@ describe('Metadata Handler', () => {
 
         describe('texts', () => {
             it('should return a string', () => {
-                expect(Meta.generateFieldValue({ type: 'text' })).to.be.a.string;
+                expect(Meta.generateFieldValue({type: 'text'})).to.be.a.string;
             });
         });
 
         describe('longtexts', () => {
             it('should return a string', () => {
-                expect(Meta.generateFieldValue({ type: 'longtext' })).to.be.a.string;
+                expect(Meta.generateFieldValue({type: 'longtext'})).to.be.a.string;
             });
         });
 
         describe('enums', () => {
             it('should return a string', () => {
-                expect(Meta.generateFieldValue({ type: 'enum' })).to.be.a.string;
+                expect(Meta.generateFieldValue({type: 'enum'})).to.be.a.string;
             });
         });
 
         describe('names', () => {
             it('should return a string', () => {
-                expect(Meta.generateFieldValue({ type: 'name' })).to.be.a.string;
+                expect(Meta.generateFieldValue({type: 'name'})).to.be.a.string;
             });
         });
     });
